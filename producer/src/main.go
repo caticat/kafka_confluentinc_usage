6007e3c26ed2fbd30b5f960893e7ffccf130d5aa
panj <catifish@163.com>
2019-05-16 17:58:19 +0800

    channel producer

--- producer/src/main.go
+++ producer/src/main.go
@@ -3,12 +3,32 @@ package main
 import (
 	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"os"
+	"os/signal"
+	"syscall"
 )
+
+var chaMsg = make(chan string, 1)
 
 func main() {
 	fmt.Println("开始")
-	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost:9092,localhost:9093,localhost:9094"})
+
+	brokerList := "localhost:9092,localhost:9093,localhost:9094"
+	topic := "panTopic"
+
+	produce(brokerList, topic)
+
+	fmt.Println("结束")
+}
+
+func produce(brokerList string, topic string) {
+
+	chaSignal := make(chan os.Signal, 1)
+	signal.Notify(chaSignal, syscall.SIGINT, syscall.SIGTERM)
+
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": brokerList})
 	if err != nil {
+		fmt.Printf("Failed to create producer: %s\n", err)
 		panic(err)
 	}
 	defer p.Close()
@@ -19,24 +39,41 @@ func main() {
 			switch ev := e.(type) {
 			case *kafka.Message:
 				if ev.TopicPartition.Error != nil {
-					fmt.Printf("Delivery failed:%v\n", ev.TopicPartition)
+					fmt.Printf("Delivery failed:%v\n", ev.TopicPartition.Error)
 				} else {
-					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+					fmt.Printf("Delivered message to %s [%d] at offset %v\n", *ev.TopicPartition.Topic, ev.TopicPartition.Partition, ev.TopicPartition.Offset)
 				}
+			default:
+				fmt.Printf("Ignored event: %s\n", ev)
 			}
 		}
 	}()
 
-	// 发送消息
-	topic := "panTopic"
-	for _, msg := range []string{"aaa", "bbb", "ccc"} {
-		err = p.Produce(&kafka.Message{
-			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-			Value:          []byte(msg),
-		}, nil)
-		fmt.Printf("发送:%v,错误:%v\n", msg, err)
-	}
+	// 输入数据
+	run := true
+	go func() {
+		for run {
+			input := ""
+			fmt.Scan(&input)
+			chaMsg <- input
+		}
+	}()
 
-	p.Flush(15 * 1000)
-	fmt.Println("结束")
+	// 发送消息
+	for run {
+		select {
+		case sig := <- chaSignal:
+			fmt.Printf("Caught signal %v: terminating\n", sig)
+			run = false
+		case msg := <- chaMsg:
+			fmt.Printf("Trying delivery msg: %v\n", msg)
+			p.ProduceChannel() <- &kafka.Message{
+				TopicPartition:kafka.TopicPartition{
+					Topic:&topic,
+					Partition:kafka.PartitionAny,
+				},
+				Value:[]byte(msg),
+			}
+		}
+	}
 }
